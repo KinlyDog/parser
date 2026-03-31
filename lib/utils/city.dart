4315ed1b8305d814dce4cd4fb7e90293bbb3884d
Andrey <kinlydog@gmail.com>
2026-03-31 14:21:55 +1000

    some changes in algoritmh

--- lib/utils/city.dart
+++ lib/utils/city.dart
@@ -5,35 +5,17 @@ Future<String> getCityFromCookie() async {
   final file = File('config/auth.json');
 
   if (!await file.exists()) {
-    return '❌ auth.json не найден';
+    return 'auth.json не найден';
   }
 
   final jsonData = jsonDecode(await file.readAsString());
   final cookie = jsonData['cookie'] as String;
 
-  // ===== 1. Пытаемся через current_path (основной способ) =====
-  final currentMatch = RegExp(r'current_path=([^;]+)').firstMatch(cookie);
-
-  if (currentMatch != null) {
-    try {
-      final decoded = Uri.decodeComponent(currentMatch.group(1)!);
-
-      final jsonMatch = RegExp(r'\{.*\}').firstMatch(decoded);
-      if (jsonMatch != null) {
-        final cityData = jsonDecode(jsonMatch.group(0)!);
-        return cityData['cityName'] ?? '❌ неизвестный город';
-      }
-    } catch (_) {
-      // игнорируем и идём дальше
-    }
-  }
-
-  // ===== 2. fallback через city_path =====
   final cityPathMatch = RegExp(r'city_path=([^;]+)').firstMatch(cookie);
 
   if (cityPathMatch != null) {
-    return cityPathMatch.group(1)!; // просто "moscow"
+    return cityPathMatch.group(1)!;
   }
 
-  return '❌ город не найден';
+  return 'Город не найден';
 }
